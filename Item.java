08a5517124faac2b63781c860f11bfc1d1d62107
Marlon Deam <marlonnobre.dev@gmail.com>
2026-05-15 08:17:45 -0300

    Implementando a classe abstrata Item

--- Item.java
+++ Item.java
@@ -1,3 +1,33 @@
-public class Item {
+public abstract class Item{
 
+    protected int id;
+    protected String titulo;
+    protected boolean disponivel;
+
+    public Item(int id,String titulo){
+        this.id = id;
+        this.titulo=titulo;
+        this.disponivel = true;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public boolean isDisponivel(){
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel){
+        this.disponivel = disponivel;
+    }
+    @Override
+
+    public String toString(){
+        return "ID: " + id +
+               " | Título: " + titulo +
+               " | Disponível: " + disponivel;
+    }
 }
